af64f70f403cc10360ccb8a3409a6765143f44b6
Shomegie <shomegie@gmail.com>
2022-04-25 23:30:33 +0100

    mobile view + logo + app color template

--- src/components/logic/index.svelte
+++ src/components/logic/index.svelte
@@ -1,67 +1,77 @@
 <script>
+    
 
-var cardValue = (i)=> i%13>10?10:i%13
 
-var cardFace = (face)=> {
-    if (face>39)
-        return "clubs"
-    else if (face>26)
-        return "spades"
-    else if (face>13)
-        return "diamonds"
-    else
-        return "hearts"
-}
+    var cardValue = (i)=> i%13>10?10:i%13
 
-/* vv card shuffler vv */
-var deck = []
-let card;
-let shuffleDeck = () => {
-    for (let i=1; i<=52; i++){
-        card = {id:i,value:cardValue(i),face:cardFace(i)}
-        deck.push(card)
+    var cardFace = (face)=> {
+        if (face>39)
+            return "clubs"
+        else if (face>26)
+            return "spades"
+        else if (face>13)
+            return "diamonds"
+        else
+            return "hearts"
     }
-}
-shuffleDeck()
-/* ^^ card shuffler ^^ */
+
+    /* vv card shuffler vv */
+    var deck = []
+    let card;
+    let shuffleDeck = () => {
+        for (let i=1; i<=52; i++){
+            card = {id:i,value:cardValue(i),face:cardFace(i)}
+            deck.push(card)
+        }
+    }
+    shuffleDeck()
+    /* ^^ card shuffler ^^ */
 
 
 
-let draw = ()=> {
-    let length = deck.length
-    let draw_pick = Math.floor((Math.random()*length))
+    let draw = ()=> {
+        let length = deck.length
+        let draw_pick = Math.floor((Math.random()*length))
 
-    return  deck.splice(draw_pick,1)
+        return  deck.splice(draw_pick,1)
 
-}
+    }
 
 
-let draws = []
-for (let i=0; i<52; i++){
-    draws.push(draw())
-}
+    let draws = []
+    for (let i=0; i<52; i++){
+        draws.push(draw())
+    }
 
 //console.log(draws)
 
 </script>
 
 
-<ul>
+<div class="app-container h-full bg-contain bg-origin-border" style= "background-image: url(images/web.png)" >
+
+    
+    <!-- <img src="images/bg2.png" alt="background image" /> -->
+
+</div>
+
+
+<!-- <ul>
     <li>Player(s) card 1</li>
     <li>Dealer card 1 face down</li>
     <li>Player(s) card 2</li>
     <li>Dealer card 2 face up</li>
-</ul>
+</ul> -->
 
 <!-- 
 {deck[27].id}
 {deck[27].value}
 {deck[27].face} -->
 
-{#each draws as card, index }
+<!-- {#each draws as card, index }
    <p class="text-red-400"> {index+": "} {Object.entries(card[0]).map(x=>x.join(": ")).join("\n")} </p>
-    <!-- {Object.entries(card[0][0]).map(x=>x.join(":")).join("\n")} <br> -->
-{/each}
+{/each} -->
+
 
 
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,21 @@
 
 </script>
 
-<div class="h-screen p-8 bg-green-900 text-white">
+<div class="app-bg h-screen text-white">
     <Logic />
 </div>
+
+<!-- background: rgb(17,32,35);
+background: linear-gradient(27deg, rgba(17,32,35,1) 0%, rgba(26,33,79,1) 87%); -->
+
+
+<style>
+    .app-bg{
+        background: rgb(0,0,0);
+        background: linear-gradient(27deg, rgb(25, 58, 63) 30%, rgb(48, 32, 50) 100%);
+        background: linear-gradient(25deg, rgb(15, 33, 36) 10%, #141a59 100%);
+        background: linear-gradient(25deg, #071517 10%, rgb(26, 20, 32) 100%);
+
+
+    }
+</style>
